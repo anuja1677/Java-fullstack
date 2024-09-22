ae1b8edd699c6073171567239dca29fbcd96dc88
anuja <anujachougule1677@gmail.com>
2024-09-22 20:00:44 +0530

    leafyear added

---
+++ core java/control statement/leafyear.java
@@ -0,0 +1,11 @@
+class leapyear{
+	public static void main(String args[]){
+		int leap = 2024;
+		if(leap%4 == 0){
+			System.out.println(leap+"is leap year");
+		}
+		else{
+			System.out.println(leap+"is not leap year");
+		}
+	}
+}
